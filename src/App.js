289c40dc011ef9edfe75dd80b1b40e445387423a
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2019-10-19 18:10:44 -0500

    working model for now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import './scss/UserForm.scss';
+import Navbar from "./components/Navbar";
+import Profile1 from "./featured/Profile1";
+import Profile2 from "./featured/Profile2";
+import Profile3 from "./featured/Profile3";
+import Profile4 from "./featured/Profile4";
+import Home from './pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import { withRouter } from "react-router";
+
+
+
+import { Switch, Route } from 'react-router-dom';
+import UserForm from './pages/UserForm';
+
+const App = (props) => {
+    
+    return (
+      <>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/Profile1" component={Profile1} />
+        <Route path="/Profile2" component={Profile2} />
+        <Route path="/Profile3" component={Profile3} />
+        <Route path="/Profile4" component={Profile4} />
+        <Route exact path="/UserForm" component={UserForm} />
+       
+      </Switch>
+      </>
+    );
+
 }
 
-export default App;
+ const mapStateToProps = (state) => {
+   return {
+     posts: state.posts,
+   }
+
+ }
+
+export default withRouter (App);
--- src/featured/Profile4.js
+++ src/featured/Profile4.js
@@ -5,19 +5,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const items = [
   {
      
-    src: 'pexels-photo-169190.jpeg', 
+    src: 'pexels-photo-1024993.jpeg', 
     altText: 'Slide 1',
     caption: ' Location: Omaha, NE',
     header: 'Eric and Joy Wedding'
   },
   {
-    src: 'pexels-photo-169198.jpeg',
+    src: 'pexels-photo-1024960.jpeg',
     altText: 'Slide 2',
     caption: 'Rustic Theme',
     header: 'Shivani is the wedding Photographer'
   },
   {
-    src: 'pexels-photo-265730.jpeg',  
+    src: 'pexels-photo-540522.jpeg',  
     altText: 'Slide 3',
     caption: 'Wedding planned at brides behset',
     header: '100 + guests'
